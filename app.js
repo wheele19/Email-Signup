c694524a2c05f56dacbdf003c1fce5d94ecf8c00
David Wheeler <78556163+wheele19@users.noreply.github.com>
2023-02-01 06:34:47 -0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,58 @@
+const fs = require('fs');
+var dotenv = require("dotenv")
+dotenv.config()
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const nodemailer = require('nodemailer');
+const emailPassword = process.env.EMAIL_PASSWORD;
+const path = require('path');
+dotenv.config()
+
+
+const app = express();
+app.use(bodyParser.json());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(express.json());
+app.use(express.urlencoded({extended: false}))
+app.post('/subscribe', (req, res) => {
+  res.set('Content-Type', 'application/json');
+  console.log(req.body);
+  const email = req.body.email;
+  console.log(email);
+  const transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+      user: 'masculinereset@gmail.com',
+      pass: emailPassword,
+    },
+  });
+
+  app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+  });
+
+
+  const mailOptions = {
+    from: 'no-reply@themasculinereset.com',
+    to: 'masculinereset@gmail.com',
+    subject: 'New subscriber for The Masculine Reset',
+    text: `A new subscriber with the email address: ${email} has signed up for The Masculine Reset.`,
+  };
+
+
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      console.log(error);
+      res.status(500).send('There was a problem, please try again.');
+    } else {
+      console.log('Email sent: ' + info.response);
+      res.send('Thank you for subscribing!');
+    }
+  });
+});
+
+app.listen(3000, () => {
+  console.log('Server is listening on port 3000');
+});
